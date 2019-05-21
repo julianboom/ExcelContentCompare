643f5de199e1025c28914f9aef080b7058af20a5
caizonglin <171900662@qq.com>
2019-05-21 12:38:16 +0800

    添加项目

---
+++ ContentCompare/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ContentCompare
+{
+    partial class BU
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(419, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "加载excel主";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(13, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 21);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(419, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "加载excel副";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "excel主 列：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "excel副 列：";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(81, 83);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(154, 21);
+            this.textBox3.TabIndex = 1;
+            this.textBox3.Text = "边控人员";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(81, 110);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(154, 21);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.Text = "姓名";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(274, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 48);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "比较数据";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(417, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "正在统计数据，请稍后。。。";
+            this.label3.Visible = false;
+            // 
+            // BU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 160);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BU";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CCC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ContentCompare/Form1.cs
@@ -0,0 +1,182 @@
+﻿using Aspose.Cells;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ContentCompare
+{
+    public partial class BU : Form
+    {
+        public BU()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = VV();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = VV();
+        }
+        public string VV()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Microsoft Excel files(*.xls)|*.xls;*.xlsx";//过滤一下，只要表格格式的
+            ofd.InitialDirectory = "c:\\";
+            ofd.RestoreDirectory = true;
+            ofd.FilterIndex = 1;
+            ofd.AddExtension = true;
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            ofd.ShowHelp = true;//是否显示帮助按钮
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                return ofd.FileName;
+            }
+            return "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.button3.Enabled = false;
+            this.label3.Visible = true;
+            new Thread(() => {
+                try
+                {
+                    List<string> tableNames1 = getNames(this.textBox1.Text);
+                    List<string> list = new List<string>();//定义主表中姓名的容器
+                    Workbook workbook = new Workbook();
+                    workbook.Open(this.textBox2.Text);
+                    int col = 0;
+                    int row = 2;
+                    Cells cells = null;
+                    geCells(workbook, ref cells, ref col, ref row);
+                    Style style = workbook.Styles[workbook.Styles.Add()];//新增样式
+                    style.HorizontalAlignment = TextAlignmentType.Center;//文字居中
+                    style.Font.Name = "宋体";//文字字体
+                                           //style.Font.Size = 14;//文字大小
+                                           //style.Font.IsBold = true;//粗体
+                    style.IsTextWrapped = true;//单元格内容自动换行
+                    style.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+                    style.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+                    style.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+                    style.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+                    style.Pattern = BackgroundType.Solid;
+                    style.ForegroundColor = Color.Red;//设置背景颜色
+                    style.Font.Color = Color.Orange;//文本颜色
+
+                    for (; row < cells.MaxDataRow + 1; row++)//遍历每行数据
+                    {
+                        MatchCollection matchCollection = Regex.Matches(cells[row, col].StringValue, "[\u4e00-\u9fa5]+");//通过正则匹配出字符串中的姓名部分
+                        if (matchCollection.Count > 0)//如果含有姓名
+                            foreach (var item in matchCollection)
+                            {
+                                string result = tableNames1.Find(f => f.Equals(item.ToString()));
+                                if (result == null)
+                                {
+                                    cells[row, col].SetStyle(style);
+                                }
+                            }
+                    }
+                    workbook.Save(this.textBox2.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("数据统计错误，请验证表格数据");
+                    return;
+                }
+                finally
+                {
+                    this.button1.Enabled = true;
+                    this.button2.Enabled = true;
+                    this.button3.Enabled = true;
+                    this.label3.Visible = false;
+                }
+                MessageBox.Show("数据统计结束，请查看表格数据");
+            })
+            {
+                IsBackground = true,
+                Priority = ThreadPriority.Highest
+            }.Start();
+        }
+
+        private List<string> getNames(string path)
+        {
+            Workbook workbook = new Workbook();
+            workbook.Open(path);
+            Cells cells = workbook.Worksheets[0].Cells;
+
+            int col = 0;//定义列，第一列开始遍历
+            int row = 1;//定义行，第二行开始遍历
+            for (; col < cells.MaxDataColumn + 1; col++)//遍历列
+            {
+
+                string s = cells[row, col].StringValue.Trim();//获取每一列
+                if (s.Equals(this.textBox3.Text))
+                {
+                    break;//定位至目标列
+                }
+            }
+            row++;//
+            List<string> list = new List<string>();//定义主表中姓名的容器
+            for (; row < cells.MaxDataRow + 1; row++)//遍历每行数据
+            {
+                MatchCollection matchCollection = Regex.Matches(cells[row, col].StringValue, "[\u4e00-\u9fa5]+");//通过正则匹配出字符串中的姓名部分
+                if (matchCollection.Count > 0)//如果含有姓名
+                    foreach (var item in matchCollection)
+                    {
+                        list.Add(item.ToString());//则将该姓名添加至集合中
+                    }
+            }
+            return list;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="cells"></param>
+        /// <param name="col">//定义列，第一列开始遍历</param>
+        /// <param name="row">//定义行，第二行开始遍历</param>
+        private void geCells(Workbook workbook, ref Cells cells,ref int col,ref int row)
+        {
+            
+            cells = workbook.Worksheets[0].Cells;
+            for (; col < cells.MaxDataColumn + 1; col++)//遍历列
+            {
+
+                string s = cells[row, col].StringValue.Trim();//获取每一列
+                if (s.Equals(this.textBox4.Text))
+                {
+                    break;//定位至目标列
+                }
+            }
+            row++;
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
